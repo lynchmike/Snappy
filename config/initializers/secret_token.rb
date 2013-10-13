1ca6ca972309ca3c1ef3129eb393281ba674f7a9
Mike Lynch <lynchmike77@yahoo.com>
2013-10-12 19:15:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makeitsnappy::Application.config.secret_key_base = '30474746d899f451a35c1bbf3b30fa178c8b53895a389f5c20e22f88040dfdb77364b815c9c8f62be3db527d2f279d7af6e3266b99f6ece11cb10d3c2920572a'
